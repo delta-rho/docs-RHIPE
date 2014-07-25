d12925d6fe0355c4b66a911ee59a3cdda17b1a1c
XiaosuTong <xiaosutong@gmail.com>
2014-07-25 14:03:15 -0400

    update timing example

---
+++ tutorial/buildDoc.R
@@ -0,0 +1,27 @@
+
+## install packages required to build the docs
+#install.packages("devtools")
+#library(devtools)
+#install_github("staticdocs", "hadley")
+#install_github("buildDocs", "hafen")
+
+
+## load the pacakge
+library(buildDocs)
+
+files <- list.files("/home/tongx/Github/docs-RHIPE/tutorial/docs")
+files <- grep('01', files, invert=T, value=T)
+## function to build the docs
+#   assuming your working directory is 
+#   doc.RHIPE/how-to-nuggets/
+buildDocs(
+   docsLoc       = "./docs",
+   outLoc        = "./",
+   copyrightText = NULL,
+   pageList      = files,
+   navPill       = packageNavPill("https://github.com/tesseradata/RHIPE"),
+   # editHref      = "https://github.com/user/project/edit/gh-pages/docs/",
+   knit          = TRUE,
+   purl          = FALSE
+)
+
